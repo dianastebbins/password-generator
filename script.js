cc053930edd75cf76aeb7b31d6573f6833b7e51e
Diana Stebbins <dianastebbins97@gmail.com>
2021-04-30 23:54:24 -0700

    basic function flow set

--- script.js
+++ script.js
@@ -10,5 +10,41 @@ function writePassword() {
 
 }
 
+var pwLength = 0;
+var allowLower = false;
+var allowUpper = false;
+var allowNumeric = false;
+var allowSpecial = false;
+
+function getCriteriaMinMax(){
+  // at least 8 characters and no more than 128 characters
+  console.log(`getCriteriaMinMax`);
+
+}
+
+function getCriteriaCharType(){
+  // choose lowercase, uppercase, numeric, and/or special characters
+  // while(){ 
+    console.log(`getCriteriaCharType`);
+    // input should be validated and at least one character type should be selected
+  // }
+  
+}
+
+function getCriteria(){
+  console.log(`getCriteria`);
+  getCriteriaMinMax();
+  getCriteriaCharType();  
+  
+}
+
+function generatePassword() {
+  getCriteria();
+
+  // password is generated that matches the selected criteria
+
+  return "1234-is-bad-password";
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
