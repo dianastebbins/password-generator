8e8b0d42197a1fcebd6813942561560e80afaa04
Diana Stebbins <dianastebbins97@gmail.com>
2021-05-01 01:49:18 -0700

    prompts for and checks of character criteria

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  console.log(`password is ${password}`);
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-  
+  passwordText.value = password;  
 }
 
 var pwLength = 0;
@@ -20,10 +18,10 @@ var allowNumeric = false;
 var allowSpecial = false;
 
 function getCriteriaMinMax(){
-  // at least 8 characters and no more than 128 characters
-  console.log(`getCriteriaMinMax`);
   while(pwLength < PW_MIN || pwLength > PW_MAX){
     pwLength = prompt(`Enter a password length (8 to 128 characters): `);
+    
+    // at least 8 characters and no more than 128 characters
     if(pwLength < PW_MIN || pwLength > PW_MAX){
       confirm(`Length must be at least 8 and no more than 128 characters. Try again.`);
     }
@@ -33,10 +31,17 @@ function getCriteriaMinMax(){
 
 function getCriteriaCharType(){
   // choose lowercase, uppercase, numeric, and/or special characters
-  // while(){ 
-    console.log(`getCriteriaCharType`);
+  while(!allowLower && !allowUpper && !allowNumeric && !allowSpecial){ 
+    allowLower = confirm(`Shall password contain lowercase letter characters?`);
+    allowUpper = confirm(`Shall password contain uppercase letter characters?`);
+    allowNumeric = confirm(`Shall password contain numeric characters?`);
+    allowSpecial = confirm(`Shall password contain special characters?`);
+  
     // input should be validated and at least one character type should be selected
-  // }
+    if(!allowLower && !allowUpper && !allowNumeric && !allowSpecial){
+      confirm(`Give me something to work with...try again.`);
+    }
+  }
   
 }
 
